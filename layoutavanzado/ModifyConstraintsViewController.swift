4f4d12286338ef8537593f11c69888f8b451959b
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-10-31 22:26:38 -0300

    [feature] modify contrainst from code

--- layoutavanzado/ModifyConstraintsViewController.swift
+++ layoutavanzado/ModifyConstraintsViewController.swift
@@ -1,29 +1,19 @@
-//
-//  ModifyConstraintsViewController.swift
-//  layoutavanzado
-//
-//  Created by Agustin Carbajal on 31/10/2023.
-//
-
 import UIKit
 
 class ModifyConstraintsViewController: UIViewController {
 
+    @IBOutlet weak var heightConstraints: NSLayoutConstraint!
+    @IBOutlet weak var widthConstraints: NSLayoutConstraint!
+    @IBOutlet weak var topConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
+        heightConstraints.constant = 300
+        topConstraint.constant = 100
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
