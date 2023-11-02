b55124cbcc013cebbcf5d2a5e29e47c0e6620751
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-11-02 18:58:26 -0300

    [feature] constraints by code

--- layoutavanzado/CreateConstraintsViewController.swift
+++ layoutavanzado/CreateConstraintsViewController.swift
@@ -1,29 +1,26 @@
-//
-//  CreateConstraintsViewController.swift
-//  layoutavanzado
-//
-//  Created by Agustin Carbajal on 02/11/2023.
-//
-
 import UIKit
 
 class CreateConstraintsViewController: UIViewController {
 
+    @IBOutlet weak var yellowView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
+        //constraints de tamaño programaticamente
+        yellowView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        yellowView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        
+        //constraints de posicion programaticamente
+        yellowView.translatesAutoresizingMaskIntoConstraints = false
+        view.addConstraint(NSLayoutConstraint(item: yellowView!, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1000, constant: 16))
+         
+        yellowView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16).isActive = true
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
